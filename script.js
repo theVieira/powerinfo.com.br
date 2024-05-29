bde929d4fe1aec902eb01bb875bd1ca7caa76b30
theVieira <gabrielvieiraguimaraes4@gmail.com>
2024-05-29 14:11:50 -0400

    fixin

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const btn = document.querySelector('#powerButton')
 
 function activateBtn() {
     if(btn.classList == 'off') {
-        btn.style.filter = 'brightness(300%)'
+        btn.style.filter = 'brightness(100%)'
         btn.classList = 'on'
     } else if(btn.classList == 'on') {
-        btn.style.filter = 'brightness(100%)'
+        btn.style.filter = 'brightness(60%)'
         btn.className = 'off'
     }
 }
